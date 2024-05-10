14ff8d3a19a7ccad4ae974b6b49f35e8a79fa119
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-05-09 23:46:53 -0400

    En la Carpeta DAL previamente creada, este está heredando de DbContext, definí el constructor y agregué la tabla llamada Técnicos

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -1,6 +1,12 @@
-﻿namespace RegistroTecnicos.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.Models;
+
+namespace RegistroTecnicos.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Tecnicos> Tecnicos { get; set; }
     }
 }
