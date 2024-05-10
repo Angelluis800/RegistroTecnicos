e31ad4b002652a493a2995503fc4c559b6561d30
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-05-09 23:23:21 -0400

    Añadí la carpeta models y cree el modelo Tecnicos, el cual tiene la llave primaria TecnicoId, tiene una propiedad llamada Nombres y otra propiedad llamada SueldoHora

---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+
+    [Required(ErrorMessage = " Este campo debe ser llenado con los datos requeridos")]
+    [RegularExpression(@"[a-zA-ZñÑ\s]")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "Este campo debe ser llenado con los datos requeridos")]
+    [Range(0.1, float.MaxValue, ErrorMessage = "Ingrese un sueldo mayor a 0")]
+    public decimal SueldoHora { get; set; }
+}
