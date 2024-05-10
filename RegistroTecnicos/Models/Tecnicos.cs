aa5d23383072915047ce1b413492ebb81c6da344
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-05-10 17:28:38 -0400

    En el componente create utilicé la clase col y la clase row para dividir y minimizar el tamaño de los campos

--- RegistroTecnicos/Models/Tecnicos.cs
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -12,6 +12,6 @@ public class Tecnicos
     public string? Nombres { get; set; }
 
     [Required(ErrorMessage = "Este campo debe ser llenado con los datos requeridos")]
-    [Range(0.1, float.MaxValue, ErrorMessage = "Ingrese un sueldo mayor a 0")]
+    [Range(0.1, 100000000, ErrorMessage = "Ingrese un sueldo mayor a 0 y menor a 100000000")]
     public decimal SueldoHora { get; set; }
 }
