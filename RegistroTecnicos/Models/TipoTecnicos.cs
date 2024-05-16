fb524fb85a354aa270726737e8902bc724763fce
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-05-16 16:27:25 -0400

    Creando modelo TipoTecnicos

---
+++ RegistroTecnicos/Models/TipoTecnicos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models;
+
+public class TipoTecnicos
+{
+    [Key]
+    public int IdTecnico { get; set; }
+
+    [Required(ErrorMessage = " Este campo debe ser llenado con los datos requeridos")]
+    [RegularExpression(@"[a-zA-ZñÑ\s]+$", ErrorMessage = "Este campo no debe contener caracteres especiales ni números")]
+    public string? Descripcion { get; set; }
+}
