5f71194696214a3f5d287ee0c0b9ea91234de18d
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-05-24 09:07:46 -0400

    Agregando Lista de Incentivos en el Módelo TiposTecnicos

--- RegistroTecnicos/Models/TiposTecnicos.cs
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -10,4 +10,5 @@ public class TiposTecnicos
     [Required(ErrorMessage = " Este campo debe ser llenado con los datos requeridos")]
     [RegularExpression(@"[a-zA-ZñÑ\s]+$", ErrorMessage = "Este campo no debe contener caracteres especiales ni números")]
     public string? Descripcion { get; set; }
+    public List<Incentivos> Incentivos { get; set; } = new List<Incentivos>();
 }
