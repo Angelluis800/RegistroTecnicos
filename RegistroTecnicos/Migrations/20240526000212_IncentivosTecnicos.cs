c0a62cf31503e5e9d5a83fa95b5730eae4773bf6
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-05-25 20:11:40 -0400

    Agregando nueva Migración

---
+++ RegistroTecnicos/Migrations/20240526000212_IncentivosTecnicos.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncentivosTecnicos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tecnicos_IncentivoId",
+                table: "Tecnicos");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TecnicoId",
+                table: "Incentivos",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tecnicos_IncentivoId",
+                table: "Tecnicos",
+                column: "IncentivoId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tecnicos_IncentivoId",
+                table: "Tecnicos");
+
+            migrationBuilder.DropColumn(
+                name: "TecnicoId",
+                table: "Incentivos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tecnicos_IncentivoId",
+                table: "Tecnicos",
+                column: "IncentivoId");
+        }
+    }
+}
